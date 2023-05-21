8b21f240479feac7bfb3985d41fdbfce449ce4da
akanevrc <akanezoranomukou@gmail.com>
2023-05-21 10:36:15 +0900

    feat: lib targetを追加

--- akanec/src/main.rs
+++ akanec/src/main.rs
@@ -3,14 +3,13 @@ mod lexer;
 mod parser;
 mod llvm;
 mod codegen;
+mod compiler;
 
 #[cfg(test)]
 mod tests;
 
-use std::fs;
 use anyhow::Result;
 use clap::Parser;
-use self::llvm::LLVM;
 
 #[derive(Parser, Debug)]
 #[command(name = "akanec", author, version, about, long_about = None)]
@@ -25,16 +24,6 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    compile(&args.input, &args.output)?;
-    Ok(())
-}
-
-fn compile(in_path: &str, out_path: &str) -> Result<()> {
-    let code = fs::read_to_string(in_path)?;
-    let tokens = lexer::lex(code)?;
-    let asts = parser::parse(tokens)?;
-    let mut llvm = LLVM::new(in_path);
-    codegen::compile(&mut llvm, &asts)?;
-    llvm.print_module_to_file(out_path)?;
+    compiler::compile(&args.input, &args.output)?;
     Ok(())
 }
