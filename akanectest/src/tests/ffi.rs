32306b759d8aadcd6430544871b159454fe31566
akanevrc <akanezoranomukou@gmail.com>
2023-07-01 12:16:22 +0900

    chore: akane-langの関数をキャメルケースに変更

--- akanectest/src/tests/ffi.rs
+++ akanectest/src/tests/ffi.rs
@@ -3,12 +3,12 @@ use std::os::raw::c_int;
 #[link(name = "akanectest")]
 extern "C" {
     pub fn one() -> c_int;
-    pub fn add_one(a: c_int) -> c_int;
-    pub fn add_nums(a: c_int, b: c_int) -> c_int;
-    pub fn double_add_nums(a: c_int, b: c_int) -> c_int;
-    pub fn add_nums_paren(a: c_int, b: c_int) -> c_int;
-    pub fn eval_add_one(a: c_int) -> c_int;
-    pub fn eval_add_nums(a: c_int, b: c_int) -> c_int;
-    pub fn mul_and_add(a: c_int, b: c_int, c: c_int, d: c_int) -> c_int;
-    pub fn mul_and_add_one(a: c_int, b: c_int) -> c_int;
+    pub fn addOne(a: c_int) -> c_int;
+    pub fn addNums(a: c_int, b: c_int) -> c_int;
+    pub fn doubleAddNums(a: c_int, b: c_int) -> c_int;
+    pub fn addNumsParen(a: c_int, b: c_int) -> c_int;
+    pub fn evalAddOne(a: c_int) -> c_int;
+    pub fn evalAddNums(a: c_int, b: c_int) -> c_int;
+    pub fn mulAndAdd(a: c_int, b: c_int, c: c_int, d: c_int) -> c_int;
+    pub fn mulAndAddOne(a: c_int, b: c_int) -> c_int;
 }
--- akanectest/src/tests/mod.rs
+++ akanectest/src/tests/mod.rs
@@ -10,63 +10,63 @@ fn one() {
 #[test]
 fn add_one() {
     unsafe {
-        assert_eq!(ffi::add_one(1), 2);
-        assert_eq!(ffi::add_one(4), 5);
+        assert_eq!(ffi::addOne(1), 2);
+        assert_eq!(ffi::addOne(4), 5);
     }
 }
 
 #[test]
 fn add_nums() {
     unsafe {
-        assert_eq!(ffi::add_nums(1, 1), 2);
-        assert_eq!(ffi::add_nums(3, 5), 8);
+        assert_eq!(ffi::addNums(1, 1), 2);
+        assert_eq!(ffi::addNums(3, 5), 8);
     }
 }
 
 #[test]
 fn double_add_nums() {
     unsafe {
-        assert_eq!(ffi::double_add_nums(1, 1), 4);
-        assert_eq!(ffi::double_add_nums(3, 5), 16);
+        assert_eq!(ffi::doubleAddNums(1, 1), 4);
+        assert_eq!(ffi::doubleAddNums(3, 5), 16);
     }
 }
 
 #[test]
 fn add_nums_paren() {
     unsafe {
-        assert_eq!(ffi::add_nums_paren(1, 1), 2);
-        assert_eq!(ffi::add_nums_paren(3, 5), 8);
+        assert_eq!(ffi::addNumsParen(1, 1), 2);
+        assert_eq!(ffi::addNumsParen(3, 5), 8);
     }
 }
 
 #[test]
 fn eval_add_one() {
     unsafe {
-        assert_eq!(ffi::eval_add_one(1), 2);
-        assert_eq!(ffi::eval_add_one(4), 5);
+        assert_eq!(ffi::evalAddOne(1), 2);
+        assert_eq!(ffi::evalAddOne(4), 5);
     }
 }
 
 #[test]
 fn eval_add_nums() {
     unsafe {
-        assert_eq!(ffi::eval_add_nums(1, 1), 2);
-        assert_eq!(ffi::eval_add_nums(3, 5), 8);
+        assert_eq!(ffi::evalAddNums(1, 1), 2);
+        assert_eq!(ffi::evalAddNums(3, 5), 8);
     }
 }
 
 #[test]
 fn mul_and_add() {
     unsafe {
-        assert_eq!(ffi::mul_and_add(1, 1, 1, 1), 2);
-        assert_eq!(ffi::mul_and_add(3, 5, 7, 9), 78);
+        assert_eq!(ffi::mulAndAdd(1, 1, 1, 1), 2);
+        assert_eq!(ffi::mulAndAdd(3, 5, 7, 9), 78);
     }
 }
 
 #[test]
 fn mul_and_add_one() {
     unsafe {
-        assert_eq!(ffi::mul_and_add_one(1, 1), 2);
-        assert_eq!(ffi::mul_and_add_one(3, 5), 16);
+        assert_eq!(ffi::mulAndAddOne(1, 1), 2);
+        assert_eq!(ffi::mulAndAddOne(3, 5), 16);
     }
 }
